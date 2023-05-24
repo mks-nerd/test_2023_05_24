8d0f5d00002769dd9d88979f6f7d472959894f54
Mohan Sahu <mks@pop-os.localdomain>
2023-05-24 23:54:00 +0530

    add basic data type

--- main.py
+++ main.py
@@ -14,28 +14,28 @@ def read_file(path: str) -> list[str] | None:
 
 def parse_log(log: str) -> tuple[datetime, str, str] | None:
     """parse string into time, user_name and action"""
-    log_parts = log.strip().split()
+    log_parts: list[str] = log.strip().split()
     if len(log_parts) != 3 or log_parts[-1] not in ["Start", "End"]:
         return None
 
     timestamp_str, user_name, action = log_parts
     try:
-        time = datetime.strptime(timestamp_str, "%H:%M:%S")
+        time: datetime = datetime.strptime(timestamp_str, "%H:%M:%S")
     except ValueError:
         return None
     return time, user_name, action
 
 
-def process_logs(logs):
-    logs_by_user = {}
-    data = {}
-    parsed_logs = [parse_log(log) for log in logs]
+def process_logs(logs: list[str]) -> dict | None:
+    logs_by_user: dict = {}
+    data: dict = {}
+    parsed_logs: list = [parse_log(log) for log in logs]
     parsed_logs = [parsed_log for parsed_log in parsed_logs if parsed_log]
 
     if len(parsed_logs) == 0:
         return None
-    first_parsed_log = parsed_logs[0][0]
-    last_parsed_log = parsed_logs[-1][0]
+    first_parsed_log: datetime = parsed_logs[0][0]
+    last_parsed_log: datetime = parsed_logs[-1][0]
 
     for parsed_log in parsed_logs:
         timestamp, user, action = parsed_log
@@ -51,8 +51,8 @@ def process_logs(logs):
             if start > end:
                 start_items.insert(0, first_parsed_log)
 
-        start_items_length = len(start_items)
-        end_items_length = len(end_items)
+        start_items_length: int = len(start_items)
+        end_items_length: int = len(end_items)
 
         while start_items_length != end_items_length:
             if start_items_length > end_items_length:
@@ -70,26 +70,26 @@ def process_logs(logs):
     return data
 
 
-def generate_report(path):
-    text_to_print = ""
-    all_logs = read_file(path=path)
+def generate_report(path: str) -> str:
+    text_to_print: str = ""
+    all_logs: list[str] | None = read_file(path=path)
     if all_logs is None:
         return "Can't Read File"
-    summary = process_logs(logs=all_logs)
+    summary: dict | None = process_logs(logs=all_logs)
     if summary is None:
         return text_to_print
     for user_name, user_data in summary.items():
-        text = f"{user_name} {len(user_data)} {int(sum(user_data))}\n"
+        text: str = f"{user_name} {len(user_data)} {int(sum(user_data))}\n"
         text_to_print += text
     text_to_print = text_to_print.strip()
     return text_to_print
 
 
-def print_report():
+def print_report() -> None:
     if len(sys.argv) < 2:
         print("Usage: python main.py <file_path>")
         sys.exit(1)
-    file_paths = sys.argv[1:]
+    file_paths: list[str] = sys.argv[1:]
     for file_path in file_paths:
         print(generate_report(path=file_path))
 
